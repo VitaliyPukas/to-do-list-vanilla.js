8be180caf832bcb853a8b94c54de5420cecfed1d
VitaliyP <pukasvitaliy@gmail.com>
2022-11-18 20:38:47 +0200

    fix style

--- js/class.js
+++ js/class.js
@@ -1,16 +1,3 @@
-// class Parent {
-//     constructor() {
-//         this.addHello()
-//     }
-//     addHello() {
-//         return alert('Hellonllknsdsdsdsdlkn')
-//     }
-// }
-// const child = new Parent("new information")
-
-// console.log(child.addHello);
-
-//! 
 const form = document.querySelector('#form');
 const taskTitle = document.querySelector('#create_title');
 const taskDescription = document.querySelector('#create_description');
@@ -54,12 +41,12 @@ function addTask(e) {
         <li id="${newTask.id}" class="list_item">
             <div class="todo_list_top">
                 <div class="list_top_left">
-                    <i class="icon-check-circle" data-action="done"></i>
+                    <i class="bi bi-circle check-item" data-action="done"></i>
                     <input class="list_title" value="${newTask.title}" readonly></input>
                 </div>
                 <div class="list_top_right">
-                    <button class="icon-edit-3" data-action="edit"></button>
-                    <button class="save-edit" data-action="save-edit">save</button>
+                    <button class="icon-edit-3 edit" data-action="edit"></button>
+                    <button class="save-edit bi bi-save edit-none none" data-action="save-edit"></button>
                     <button class="icon-trash-2-1" data-action="delete"></button>
                 </div>
             </div>
@@ -94,31 +81,48 @@ function deleteTask(e) {
 function editTask(e) {
     const parentItem = e.target.closest('li');
     const taskTitle = parentItem.querySelector('.list_title');
+    const saveEdit = parentItem.querySelector('.save-edit');
+    const edit = parentItem.querySelector('.edit');
     const taskDescription = parentItem.querySelector('.todo_list_bottom');
     const carentlyItem = tasks.find(task => task.id === +parentItem.id);
 
     if (e.target.dataset.action === "edit") {
-        console.log("edit");
         console.log(carentlyItem.title);
         carentlyItem.title = taskTitle.value;
         taskTitle.removeAttribute("readonly");
         taskTitle.focus();
         carentlyItem.description = taskDescription.value;
         taskDescription.removeAttribute("readonly");
+
+        edit.classList.add('none');
+        
+        saveEdit.classList.remove('none');
     }
     if (e.target.dataset.action === "save-edit") {
-        console.log("save-edit");
         taskTitle.setAttribute("readonly", true);
         taskDescription.setAttribute("readonly", true);
+        edit.classList.remove('none');
+        saveEdit.classList.add('none');
     }
 }
 
 function checkTask(e) {
     if (e.target.dataset.action === "done") {
-        const parentIte = e.target.closest('li');
-        const checkItem = parentIte.querySelector('.icon-check-circle');
-        const checkTitle = parentIte.querySelector('.list_title');
-        checkTitle.classList.add('check_title');
+        const parentItem = e.target.closest('li');
+        const checkItem = parentItem.querySelector('.check-item');
+        const checkTitle = parentItem.querySelector('.list_title');
+        const checkDescription = parentItem.querySelector('.todo_list_bottom');
         checkItem.classList.toggle('done');
+        if (checkItem.classList.contains('done')) {
+            checkItem.classList.add('bi-check-circle');
+            checkItem.classList.remove('bi-circle');
+            checkTitle.classList.add('crossed');
+            checkDescription.classList.add('crossed');
+        }else{
+            checkItem.classList.remove('bi-check-circle');
+            checkItem.classList.add('bi-circle');
+            checkTitle.classList.remove('crossed');
+            checkDescription.classList.remove('crossed');
+        }
     }
 }
