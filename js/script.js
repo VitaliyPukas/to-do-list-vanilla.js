9208413162b30086892cb2a9238007b7f857dc8c
VitaliyP <pukasvitaliy@gmail.com>
2022-11-22 09:32:14 +0200

    localStorage

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ const taskLists = document.querySelector('#todo_list');
 
 let tasks = [];
 
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach((task) => renderTask(task));
+}
+
+
+
 // Додавання задач
 form.addEventListener('submit', addTask);
 
@@ -31,30 +38,14 @@ function addTask(e) {
         id: Date.now(),
         title: titleInput,
         description: descriptionInput,
-        done: false
-    }
+        done: false,
+    };
 
     tasks.push(newTask);
 
+    saveToLocalStorage();
 
-    const taskHTML = `
-        <li id="${newTask.id}" class="list_item">
-            <div class="todo_list_top">
-                <div class="list_top_left">
-                    <i class="bi bi-circle check-item" data-action="done"></i>
-                    <input class="list_title" value="${newTask.title}" readonly></input>
-                </div>
-                <div class="list_top_right">
-                    <button class="icon-edit-3 edit" data-action="edit"></button>
-                    <button class="save-edit bi bi-save edit-none none" data-action="save-edit"></button>
-                    <button class="icon-trash-2-1" data-action="delete"></button>
-                </div>
-            </div>
-            <input class="todo_list_bottom" value="${newTask.description}" readonly></input>
-        </li>`
-
-    // Додаємо задачу в список
-    taskLists.insertAdjacentHTML('beforeend', taskHTML);
+    renderTask(newTask);
 
     // Очищаємо поле вводу
     taskTitle.value = '';
@@ -65,8 +56,12 @@ function addTask(e) {
 function deleteTask(e) {
     if (e.target.dataset.action === 'delete') {
         const parentItem = e.target.closest('li');
+        const id = Number(parentItem.id);
+        tasks = tasks.filter((task) => task.id !== id);
+        saveToLocalStorage();
         parentItem.remove();
     }
+
 }
 
 function editTask(e) {
@@ -77,16 +72,14 @@ function editTask(e) {
     const taskDescription = parentItem.querySelector('.todo_list_bottom');
     const carentlyItem = tasks.find(task => task.id === +parentItem.id);
 
+
     if (e.target.dataset.action === "edit") {
-        console.log(carentlyItem.title);
         carentlyItem.title = taskTitle.value;
         taskTitle.removeAttribute("readonly");
         taskTitle.focus();
         carentlyItem.description = taskDescription.value;
         taskDescription.removeAttribute("readonly");
-
         edit.classList.add('none');
-        
         saveEdit.classList.remove('none');
     }
     if (e.target.dataset.action === "save-edit") {
@@ -94,7 +87,10 @@ function editTask(e) {
         taskDescription.setAttribute("readonly", true);
         edit.classList.remove('none');
         saveEdit.classList.add('none');
+
+        saveToLocalStorage();
     }
+    
 }
 
 function checkTask(e) {
@@ -104,17 +100,43 @@ function checkTask(e) {
         const checkTitle = parentItem.querySelector('.list_title');
         const checkDescription = parentItem.querySelector('.todo_list_bottom');
         checkItem.classList.toggle('done');
+
         if (checkItem.classList.contains('done')) {
             checkItem.classList.add('bi-check-circle');
             checkItem.classList.remove('bi-circle');
             checkTitle.classList.add('crossed');
             checkDescription.classList.add('crossed');
-        }else{
+        } else {
             checkItem.classList.remove('bi-check-circle');
             checkItem.classList.add('bi-circle');
             checkTitle.classList.remove('crossed');
             checkDescription.classList.remove('crossed');
         }
     }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function renderTask(task) {
+    const taskHTML = `
+    <li id="${task.id}" class="list_item">
+        <div class="todo_list_top">
+            <div class="list_top_left">
+                <i class="bi bi-circle check-item" data-action="done"></i>
+                <input class="list_title" value="${task.title}" readonly></input>
+            </div>
+            <div class="list_top_right">
+                <button class="icon-edit-3 edit" data-action="edit"></button>
+                <button class="save-edit bi bi-save edit-none none" data-action="save-edit"></button>
+                <button class="icon-trash-2-1" data-action="delete"></button>
+            </div>
+        </div>
+        <input class="todo_list_bottom" value="${task.description}" readonly></input>
+    </li>`
+
+    // Додаємо задачу в список
+    taskLists.insertAdjacentHTML('beforeend', taskHTML);
 }
 
